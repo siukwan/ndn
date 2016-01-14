d0cb3b27c00cd85894c8a39ad83db049019c41c4
siukwan <siukwan@users.noreply.github.com>
2016-01-14 10:18:53 +0800

    增加兴趣树的节点和插入函数

--- ns-3/scratch/nrndn_test/ndn-InterestTree.h
+++ ns-3/scratch/nrndn_test/ndn-InterestTree.h
@@ -37,6 +37,34 @@ namespace nrndn
  */
 class NrInterestTreeImpl
 {
+public:
+	//树节点
+	struct InterestTreeNode{
+		std::map<std::string, InterestTreeNode* > child;//孩子节点
+		map<int,bool> NodeId;//感兴趣的节点
+		string lane;//当前节点的路段
+		//两个简单的构造函数
+		InterestTreeNode(std::string x) :lane(x),NodeId(-1){};
+		InterestTreeNode() :lane(""),NodeId(-1){};
+	};
+
+	void insertInterest(int&id,int pos,const std::vector<std::string>& route,InterestTreeNode* root)
+	{
+		if(pos == route.size()) return;//已经遍历完毕
+		else
+		{
+			//如果没有找到孩子
+			if( root->child.find( route[pos] ) ==root->child.end())
+				root->child[route[pos]]=new InterestTreeNode(route[pos]);
+			root->child[route[pos]]->NodeId[pos]=true;
+
+			pos++;
+			//递归地插入
+			insertInterest(id,pos,routeroot->child[route[pos]]);
+		}
+	}
+
+
 };
 
 
