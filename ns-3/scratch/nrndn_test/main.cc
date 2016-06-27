96d17422c59c5e1d07abe47aeb5f904672044859
siukwan Mac <pypy39353@qq.com>
2016-06-27 15:55:16 +0800

    修改数据包数量

--- ns-3/scratch/nrndn_test/main.cc
+++ ns-3/scratch/nrndn_test/main.cc
@@ -556,7 +556,7 @@ nrndnExample::LoadTraffic()
 	size = mobility->GetNodeSize();
 	std::cout<<"节点size："<<size<<std::endl;
 
-	accidentNum=size*4;
+	accidentNum=1;//size*4;
 	std::cout<<"(main.cc)修改accidentNum为size的4倍"<<accidentNum<<std::endl;
 
 }
