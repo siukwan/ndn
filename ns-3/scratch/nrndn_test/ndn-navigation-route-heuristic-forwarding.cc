491f60ec44aef5a61445865a619c2559b29e3aea
siukwan Mac <pypy39353@qq.com>
2016-06-30 21:28:22 +0800

    test

--- ns-3/scratch/nrndn_test/ndn-navigation-route-heuristic-forwarding.cc
+++ ns-3/scratch/nrndn_test/ndn-navigation-route-heuristic-forwarding.cc
@@ -626,6 +626,7 @@ void NavigationRouteHeuristic::OnData(Ptr<Face> face, Ptr<Data> data)
 	if(Face::APPLICATION && face->GetFlags())
 	{
 		NS_LOG_DEBUG("Get data packet from APPLICATION");
+		
 		// This is the source data from the upper node application (eg, nrProducer) of itself
 		// 1.Set the payload
 		Ptr<Packet> payload = GetNrPayload(HeaderHelper::CONTENT_OBJECT_NDNSIM,data->GetPayload(),999999999,data->GetName());
