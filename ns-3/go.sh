c14da9108377adff533bc148f91f7a4ac084cd7f
siukwan@ubuntu513 <pypy39353@qq.com>
2016-07-15 21:43:47 +0800

    update go.sh

--- ns-3/go.sh
+++ ns-3/go.sh
@@ -16,41 +16,42 @@ file_name=$shell_date"-"$vehicle_num"nodes_"$program_name
 #输出shell_data
 echo $file_name
 
-mkdir ~/$file_name
+mkdir ~/tmp
+mkdir ~/tmp/$file_name
 
 cd ~/ndn/ns-3/
 #先进行编译
 ./waf --run "$program_name --method=3"
 
 #后台运行ndn
-./waf --run "$program_name --method=0" > ~/$file_name/ndn_record.txt &
+./waf --run "$program_name --method=0" > ~/tmp/$file_name/ndn_record.txt &
 #后台运行dis
 sleep 4
-./waf --run "$program_name --method=1" > ~/$file_name/dis_record.txt &
+./waf --run "$program_name --method=1" > ~/tmp/$file_name/dis_record.txt &
 #后台运行cds
 sleep 4
-./waf --run "$program_name --method=2" > ~/$file_name/cds_record.txt &
+./waf --run "$program_name --method=2" > ~/tmp/$file_name/cds_record.txt &
 
 #等待后台程序结束
 wait
 
 #把结果合并输出到result.txt
-cat ~/input/NR-NDN-Simulation/result.txt ~/input/Dist-Simulation/result.txt  ~/input/CDS-Simulation/result.txt > ~/$file_name/result.txt
+cat ~/input/NR-NDN-Simulation/result.txt ~/input/Dist-Simulation/result.txt  ~/input/CDS-Simulation/result.txt > ~/tmp/$file_name/result.txt
 
 #追加，统一显示实验结果
-echo "//NDN DIST CDS" >> ~/$file_name/result.txt
-tail -2 ~/input/NR-NDN-Simulation/result.txt >> ~/$file_name/result.txt
-tail -1 ~/input/Dist-Simulation/result.txt   >> ~/$file_name/result.txt
-tail -1 ~/input/CDS-Simulation/result.txt    >> ~/$file_name/result.txt
+echo "//NDN DIST CDS" >> ~/tmp/$file_name/result.txt
+tail -2 ~/input/NR-NDN-Simulation/result.txt >> ~/tmp/$file_name/result.txt
+tail -1 ~/input/Dist-Simulation/result.txt   >> ~/tmp/$file_name/result.txt
+tail -1 ~/input/CDS-Simulation/result.txt    >> ~/tmp/$file_name/result.txt
 
-cp ~/$file_name/result.txt ~/$file_name/README.md
+cp ~/tmp/$file_name/result.txt ~/tmp/$file_name/README.md
 
 #先去更新ndc-ns3-result文件夹
 cd ~/ndc-ns3-result
 git pull
 
 #把文件夹复制到github文件夹中
-mv ~/$file_name ~/ndc-ns3-result/$file_name
+mv ~/tmp/$file_name ~/ndc-ns3-result/$file_name
 
 cd ~/ndc-ns3-result
 #git add $file_name/ndn_record.txt
