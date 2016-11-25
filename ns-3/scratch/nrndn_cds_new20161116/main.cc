964bd2f1046fa56384496cca1682052858b5e4eb
siukwan <siukwan@users.noreply.github.com>
2016-11-25 16:46:07 +0800

    修改添加事件bug

--- ns-3/scratch/nrndn_cds_new20161116/main.cc
+++ ns-3/scratch/nrndn_cds_new20161116/main.cc
@@ -874,8 +874,7 @@ void nrndnExample::InstallTraffics()
 	*/
 
 	
-	for(uint32_t i=0;i<accidentNum;++i)
-	{
+
 		for(uint32_t index = 0; index < 15; index ++)
 		{
 			Ptr<ns3::ndn::nrndn::nrProducer> producer= DynamicCast<ns3::ndn::nrndn::nrProducer>(
@@ -883,7 +882,6 @@ void nrndnExample::InstallTraffics()
 			NS_ASSERT(producer);
 			producer->addAccident(1);
 		}
-	}
 
 	std::cout<<"插入事件：完毕"<<endl;
 	getchar();
