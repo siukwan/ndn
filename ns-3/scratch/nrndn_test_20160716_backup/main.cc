c9d810b71d2f249162f0bf587dbd5a0141ba5a54
siukwan <siukwan@users.noreply.github.com>
2017-01-05 21:42:07 +0800

    25固定点

--- ns-3/scratch/nrndn_test_20160716_backup/main.cc
+++ ns-3/scratch/nrndn_test_20160716_backup/main.cc
@@ -870,7 +870,7 @@ void nrndnExample::InstallTraffics()
 		producer->addAccident(1);
 	}*/
 
-		for(uint32_t index = 0; index < 20; index ++)
+		for(uint32_t index = 0; index < 25; index ++)
 		{
 			Ptr<ns3::ndn::nrndn::nrProducer> producer= DynamicCast<ns3::ndn::nrndn::nrProducer>(
 					nodes.Get(index)->GetApplication(nrUtils::appIndex["ns3::ndn::nrndn::nrProducer"]));
