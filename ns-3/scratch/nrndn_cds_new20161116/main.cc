0b697c7f2a74acd72cd4255804a7ea96bc191270
siukwan <siukwan@users.noreply.github.com>
2017-02-21 13:59:49 +0800

    15秒

--- ns-3/scratch/nrndn_cds_new20161116/main.cc
+++ ns-3/scratch/nrndn_cds_new20161116/main.cc
@@ -893,7 +893,7 @@ void nrndnExample::InstallTraffics()
 			Ptr<ns3::ndn::nrndn::nrProducer> producer= DynamicCast<ns3::ndn::nrndn::nrProducer>(
 					nodes.Get(index)->GetApplication(nrUtils::appIndex["ns3::ndn::nrndn::nrProducer"]));
 			NS_ASSERT(producer);
-			producer->addAccident(10);
+			producer->addAccident(15);
 		}
 
 	std::cout<<"插入事件：完毕"<<endl;
