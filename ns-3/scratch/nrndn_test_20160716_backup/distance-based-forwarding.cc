aec784f275fad259f3d4045a4f10fee317d03d26
siukwan <siukwan@users.noreply.github.com>
2017-02-25 18:18:58 +0800

    test

--- ns-3/scratch/nrndn_test_20160716_backup/distance-based-forwarding.cc
+++ ns-3/scratch/nrndn_test_20160716_backup/distance-based-forwarding.cc
@@ -179,7 +179,7 @@ void DistanceBasedForwarding::OnData(Ptr<Face> face, Ptr<Data> data)
 	FwHopCountTag hopCountTag;
 	nrPayload->PeekPacketTag(hopCountTag);
 	bool isTTLReachMax = (hopCountTag.Get() > m_TTLMax);
-
+	cout << m_node->GetId() << " " << hopCountTag.Get() << " " << m_TTLMax << endl;
 	if (isTTLReachMax)
 	{
 		NS_LOG_DEBUG(
